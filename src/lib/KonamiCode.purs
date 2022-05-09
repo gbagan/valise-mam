ac8d348a3038943756a4eb0da7e86ae39456f44d
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2022-05-09 17:16:33 +0200

    simplification de konamiCode

--- src/lib/KonamiCode.purs
+++ src/lib/KonamiCode.purs
@@ -3,14 +3,12 @@ import MyPrelude
 import Data.Array (takeEnd)
 import Lib.Update (get)
 
-codeSequence ∷ String
-codeSequence = "ArrowUp ArrowUp ArrowDown ArrowDown ArrowLeft ArrowRight ArrowLeft ArrowRight b a"
+codeSequence ∷ Array String
+codeSequence = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"]
 
 konamiCode ∷ ∀m st. MonadState st m ⇒ Lens' st (Array String) → m Unit → String → m Unit
 konamiCode lens onActivation key = do
     state ← get
-    let seq = state ^. lens
-                # (_ `snoc` key)
-                # takeEnd 10
+    let seq = takeEnd 10 $ (state ^. lens) `snoc` key
     lens .= seq
-    when (joinWith " " seq == codeSequence) onActivation
+    when (seq == codeSequence) onActivation
